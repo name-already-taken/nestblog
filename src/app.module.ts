4574794a045732be8619ec80989237e350726d64
Yiwei Zhu <yzhu112@jhu.edu>
2022-05-10 00:30:48 -0400

    delete: delete module and reconstruct code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   debug: false,
-    //   playground: false,
-    // }),
     TypeOrmModule.forRoot(),
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private connection: Connection){}
+  constructor(private connection: Connection) {}
 }
